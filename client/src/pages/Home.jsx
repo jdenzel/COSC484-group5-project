a583891e5566a8a67f77466ff05ac43f4dd32a97
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-03-31 22:10:03 -0400

    Added section style rule. Finished with basic design system

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,30 @@
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <h2>Home</h2>
-            <h3>Home</h3>
-            <h4>Home</h4>
-            <h5>Home</h5>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            
-            <button className="btn btn-primary">Submit</button>
-            <button className="btn btn-secondary">Open your ledger</button>
-            <button className="btn btn-outline">Submit</button>
+            <section>
+                <h1>Home</h1>
+                <h2>Home</h2>
+                <h3>Home</h3>
+                <h4>Home</h4>
+                <h5>Home</h5>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                
+                <button className="btn btn-primary">Submit</button>
+                <button className="btn btn-secondary">Open your ledger</button>
+                <button className="btn btn-outline">Submit</button>
+            </section>
+            <section>
+                <h1>Home</h1>
+                <h2>Home</h2>
+                <h3>Home</h3>
+                <h4>Home</h4>
+                <h5>Home</h5>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                
+                <button className="btn btn-primary">Submit</button>
+                <button className="btn btn-secondary">Open your ledger</button>
+                <button className="btn btn-outline">Submit</button>
+            </section>
         </div>
     );
 }
