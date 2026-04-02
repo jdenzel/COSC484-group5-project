11a5b1b460409e93ede784f756d6de5cbf35e300
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-04-01 22:14:01 -0400

    adjusted theme spacing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,18 +3,21 @@ import "./styles/Home.css"
 
 export default function Home() {
     return (
-        <div className="hero">
-            <div className="hero-description-container">
-                <h1>Finance Management</h1>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <div className="hero-cta-btn">
-                    <button className="btn btn-secondary">Learn More</button>
-                    <button className="btn btn-outline">Get In Touch</button>
+        <section>
+            <div className="hero">
+                <div className="hero-description-container">
+                    <h1>Finance Management</h1>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <div className="hero-cta-btn">
+                        <button className="btn btn-secondary">Learn More</button>
+                        <button className="btn btn-outline">Get In Touch</button>
+                    </div>
+                </div>
+                <div className="hero-img-container">
+                    <img src={HeroImage} />
                 </div>
             </div>
-            <div className="hero-img-container">
-                <img src={HeroImage} />
-            </div>
-        </div>
+            
+        </section>
     );
 }
